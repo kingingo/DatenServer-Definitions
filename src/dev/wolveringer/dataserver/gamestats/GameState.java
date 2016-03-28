cf6c88d85340469ae5a9845efcc28e680133dbbb
root <root@mcgalaxy.de>
2016-03-28 14:49:10 +0200

    Class moved

---
+++ src/dev/wolveringer/dataserver/gamestats/GameState.java
@@ -0,0 +1,16 @@
+package dev.wolveringer.dataserver.gamestats;
+
+public enum GameState {
+	Laden("Laden"), LobbyPhase("LobbyPhase"), SchutzModus("SchutzModus"), DeathMatch("DeathMatch"), StartDeathMatch("StartDeathMatch"), StartGame("StartGame"), InGame("InGame"), Restart("Restart"), MultiGame("MultiGame"), NONE("NONE");
+
+	String state;
+
+	private GameState(String state) {
+		this.state = state;
+	}
+
+	public String string() {
+		return state;
+	}
+
+}
