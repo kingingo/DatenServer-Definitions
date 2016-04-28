d56b3a525eb2d2413ea19f83d289a58992acf6ff
Janmm14 <computerjanimaus@yahoo.de>
2016-04-28 21:30:12 +0200

    Add GameTypes PLAYER_TEAMS_PVP, PLAYER_TEAMS_SKYBLOCK, PLAYER_TEAMS_WARZ. Add optional teamType variable for GameType. Use @AllArgsaConstructor and @Getter on class level.

--- src/dev/wolveringer/dataserver/gamestats/GameType.java
+++ src/dev/wolveringer/dataserver/gamestats/GameType.java
@@ -1,33 +1,39 @@
 package dev.wolveringer.dataserver.gamestats;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum GameType {
-	GUNGAME(true, "GunGame-Server", "GunGame", ServerType.GUNGAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LEVEL },true),
-	SurvivalGames1vs1(false, "SurvivalGames1vs1", "SG1vs1", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	BedWars1vs1(false, "BedWars1vs1", "BW1vs1", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.BEDWARS_ZERSTOERTE_BEDs },true),
-	SkyWars1vs1(false, "SkyWars1vs1", "SkyWars1vs1", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LOSE, StatsKey.WIN },true),
-	Versus(false, "VERSUS", "VS", ServerType.GAME, new StatsKey[] { StatsKey.KIT_RANDOM, StatsKey.KIT_ID, StatsKey.TEAM_MAX, StatsKey.TEAM_MIN, StatsKey.ELO, StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	SkyWars(true, "SkyWars", "SkyWars", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LOSE, StatsKey.WIN },true),
-	QuickSurvivalGames(true, "QuickSurvivalGames", "QSG", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	SurvivalGames(true, "SurvivalGames", "SG", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	OneInTheChamber(true, "OneInTheChamber", "OITC", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	SkyPvP(true, "SkyPvP", "SK", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	Falldown(true, "Falldown", "FD", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.POWER, StatsKey.WIN, StatsKey.LOSE },true),
-	TroubleInMinecraft(true, "TroubleInMinecraft", "TTT", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.TTT_KARMA, StatsKey.TTT_PAESSE, StatsKey.TTT_TESTS, StatsKey.TTT_TRAITOR_PUNKTE, StatsKey.TTT_DETECTIVE_PUNKTE },true),
-	DeathGames(true, "DeathGames", "DG", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true),
-	BedWars(true, "BedWars", "BW", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.BEDWARS_ZERSTOERTE_BEDs },true),
-	SheepWars(true, "SheepWars", "SW", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.SHEEPWARS_KILLED_SHEEPS },true),
-	PVP(true, "PvP-Server", "PvP", ServerType.PVP, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.MONEY, StatsKey.ELO, StatsKey.TIME_ELO, StatsKey.TIME },true),
-	SKYBLOCK(true, "SkyBlock", "Sky", ServerType.SKYBLOCK, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.MONEY },true),
-	WARZ(true, "WarZ-Server", "WarZ", ServerType.WARZ, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.ANIMAL_KILLS, StatsKey.ANIMAL_DEATHS, StatsKey.MONSTER_KILLS, StatsKey.MONSTER_DEATHS },true),
-	CaveWars(true, "CaveWars", "CW", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.SHEEPWARS_KILLED_SHEEPS },true),
-	Masterbuilders(true, "Master Builders", "MB", ServerType.GAME, new StatsKey[] { StatsKey.LOSE, StatsKey.WIN},true),
-	Money(true, "Money", "MONEY", ServerType.BUNGEECORD, new StatsKey[] { StatsKey.COINS, StatsKey.GEMS },false),
-	NONE(true, "NONE", "FAIL", ServerType.GAME, null,false),
-	ALL(true, "NONE", "FAIL", ServerType.GAME, null,false),
-	VERSUS_KITS(false,"vs_kits_error","VS_kits",ServerType.GAME,new StatsKey[] { StatsKey.VERSUS_ID, StatsKey.VERSUS_CONTENT,StatsKey.VERSUS_ARMOR_CONTENT},false),
-	PLAYER_TEAMS_ACARDE(false,"playerteams_acarde","playerteams_acarde",ServerType.GAME,new StatsKey[] { StatsKey.TEAM_ID},false);
+	PLAYER_TEAMS_ACARDE(false,"playerteams_acarde","playerteams_acarde",ServerType.GAME,new StatsKey[] { StatsKey.TEAM_ID},false, null),
+	PLAYER_TEAMS_PVP(false,"playerteams_pvp","playerteams_pvp",ServerType.GAME,new StatsKey[] { StatsKey.TEAM_ID},false, null),
+	PLAYER_TEAMS_SKYBLOCK(false,"playerteams_skyblock","playerteams_skyblock",ServerType.GAME,new StatsKey[] { StatsKey.TEAM_ID},false, null),
+	PLAYER_TEAMS_WARZ(false,"playerteams_warz","playerteams_warz",ServerType.GAME,new StatsKey[] { StatsKey.TEAM_ID},false, null),
+	GUNGAME(true, "GunGame-Server", "GunGame", ServerType.GUNGAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LEVEL },true, null),
+	SurvivalGames1vs1(false, "SurvivalGames1vs1", "SG1vs1", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, null),
+	BedWars1vs1(false, "BedWars1vs1", "BW1vs1", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.BEDWARS_ZERSTOERTE_BEDs },true, null),
+	SkyWars1vs1(false, "SkyWars1vs1", "SkyWars1vs1", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LOSE, StatsKey.WIN },true, null),
+	Versus(false, "VERSUS", "VS", ServerType.GAME, new StatsKey[] { StatsKey.KIT_RANDOM, StatsKey.KIT_ID, StatsKey.TEAM_MAX, StatsKey.TEAM_MIN, StatsKey.ELO, StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, null),
+	SkyWars(true, "SkyWars", "SkyWars", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LOSE, StatsKey.WIN },true, PLAYER_TEAMS_ACARDE),
+	QuickSurvivalGames(true, "QuickSurvivalGames", "QSG", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, PLAYER_TEAMS_ACARDE),
+	SurvivalGames(true, "SurvivalGames", "SG", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, PLAYER_TEAMS_ACARDE),
+	OneInTheChamber(true, "OneInTheChamber", "OITC", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, PLAYER_TEAMS_ACARDE),
+	SkyPvP(true, "SkyPvP", "SK", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, PLAYER_TEAMS_ACARDE),
+	Falldown(true, "Falldown", "FD", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.POWER, StatsKey.WIN, StatsKey.LOSE },true, PLAYER_TEAMS_ACARDE),
+	TroubleInMinecraft(true, "TroubleInMinecraft", "TTT", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.TTT_KARMA, StatsKey.TTT_PAESSE, StatsKey.TTT_TESTS, StatsKey.TTT_TRAITOR_PUNKTE, StatsKey.TTT_DETECTIVE_PUNKTE },true, PLAYER_TEAMS_ACARDE),
+	DeathGames(true, "DeathGames", "DG", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE },true, PLAYER_TEAMS_ACARDE),
+	BedWars(true, "BedWars", "BW", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.BEDWARS_ZERSTOERTE_BEDs },true, PLAYER_TEAMS_ACARDE),
+	SheepWars(true, "SheepWars", "SW", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.SHEEPWARS_KILLED_SHEEPS },true, PLAYER_TEAMS_ACARDE),
+	PVP(true, "PvP-Server", "PvP", ServerType.PVP, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.MONEY, StatsKey.ELO, StatsKey.TIME_ELO, StatsKey.TIME },true,PLAYER_TEAMS_PVP),
+	SKYBLOCK(true, "SkyBlock", "Sky", ServerType.SKYBLOCK, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.MONEY },true, PLAYER_TEAMS_SKYBLOCK),
+	WARZ(true, "WarZ-Server", "WarZ", ServerType.WARZ, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.ANIMAL_KILLS, StatsKey.ANIMAL_DEATHS, StatsKey.MONSTER_KILLS, StatsKey.MONSTER_DEATHS },true, PLAYER_TEAMS_WARZ),
+	CaveWars(true, "CaveWars", "CW", ServerType.GAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.WIN, StatsKey.LOSE, StatsKey.SHEEPWARS_KILLED_SHEEPS },true, PLAYER_TEAMS_ACARDE),
+	Masterbuilders(true, "Master Builders", "MB", ServerType.GAME, new StatsKey[] { StatsKey.LOSE, StatsKey.WIN},true, PLAYER_TEAMS_ACARDE),
+	Money(true, "Money", "MONEY", ServerType.BUNGEECORD, new StatsKey[] { StatsKey.COINS, StatsKey.GEMS },false, null),
+	NONE(true, "NONE", "FAIL", ServerType.GAME, null,false, null),
+	ALL(true, "NONE", "FAIL", ServerType.GAME, null,false, null),
+	VERSUS_KITS(false,"vs_kits_error","VS_kits",ServerType.GAME,new StatsKey[] { StatsKey.VERSUS_ID, StatsKey.VERSUS_CONTENT,StatsKey.VERSUS_ARMOR_CONTENT},false, null);
 	
 	/*
 	GUNGAME_TEAMS(true, "GunGame-Server", "teams_GunGame", ServerType.GUNGAME, new StatsKey[] { StatsKey.KILLS, StatsKey.DEATHS, StatsKey.LEVEL },true),
@@ -55,28 +61,14 @@ public enum GameType {
 	ALL_TEAMS(true, "NONE", "teams_FAIL", ServerType.GAME, null,false),
 	VERSUS_KITS_TEAMS(false,"vs_kits_error","teams_VS_kits",ServerType.GAME,new StatsKey[] { StatsKey.VERSUS_ID, StatsKey.VERSUS_CONTENT,StatsKey.VERSUS_ARMOR_CONTENT},false);
 	*/
-	
-	@Getter
-	private String typ;
-	@Getter
-	private String shortName;
-	@Getter
-	private StatsKey[] stats;
-	@Getter
-	private ServerType serverType;
-	@Getter
+
 	private boolean solo = true;
-	@Getter
+	private String typ;
+	private String shortName;
+	private ServerType serverType;
+	private StatsKey[] stats;
 	private boolean arcade;
-	
-	private GameType(boolean solo, String Typ, String Kuerzel, ServerType serverType, StatsKey[] stats,boolean arcadeGame) {
-		this.typ = Typ;
-		this.solo = solo;
-		this.stats = stats;
-		this.shortName = Kuerzel;
-		this.serverType = serverType;
-		this.arcade = arcadeGame;
-	}
+	private GameType teamType;
 
 	public static GameType get(String g) {
 		g = g.replaceAll("-", "");
