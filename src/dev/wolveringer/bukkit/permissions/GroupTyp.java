72e9c615ba8660b2d8d55604b7a9adee38c607ba
WolverinDEV <markus.hadenfeldt@web.de>
2016-06-08 22:39:00 +0200

    Update

--- src/dev/wolveringer/bukkit/permissions/GroupTyp.java
+++ src/dev/wolveringer/bukkit/permissions/GroupTyp.java
@@ -1,31 +1,27 @@
 package dev.wolveringer.bukkit.permissions;
 
 public enum GroupTyp {
-ALL("all"),
-GAME("game"),
-PVP("pvp"),
-SKY("sky"),
-WARZ("warz"),
-CREATIVE("creative");
+	ALL("all"), GAME("game"), PVP("pvp"), SKY("sky"), WARZ("warz"), CREATIVE("creative");
 
-public static GroupTyp get(String perm){
-	GroupTyp per=null;
-	for(GroupTyp permission : GroupTyp.values()){
-		if(permission.getName().equalsIgnoreCase(perm)){
-			per=permission;
-			break;
+	public static GroupTyp get(String perm) {
+		GroupTyp per = null;
+		for (GroupTyp permission : GroupTyp.values()) {
+			if (permission.getName().equalsIgnoreCase(perm)) {
+				per = permission;
+				break;
+			}
 		}
+		return per;
 	}
-	return per;
-}
 
-private String name;
-private GroupTyp(String name){
-	this.name=name;
-}
+	private String name;
 
-public String getName(){
-	return this.name;
-}
+	private GroupTyp(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 
 }
